3ff9b26ed6c013f8543548a4608da445a79d2628
noureddein <noureddein@gmail.com>
2023-03-04 00:11:16 +0300

    change files locations

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,14 @@
 ﻿import { Link } from 'react-router-dom'
 
-import Part1 from "./Part_1";
-import Part2 from "./Part_2";
-import Part3 from "./Part_3";
-import User from './User';
+import Part1 from "./useEffect/Part_1";
+import Part2 from "./useEffect/Part_2";
+import Part3 from "./useEffect/Part_3";
 
 export default function Main() {
   return (
     <div>
         <h5><Link to="/posts">Go to Posts page</Link></h5>
-        {/* <User /> */}
+        <Part3 />
     </div>
   )
 }
